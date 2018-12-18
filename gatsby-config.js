fcd5db1ea120341ac48db1d3de356edb0cad3fd8
Taylor Snead <taylorsnead@gmail.com>
2018-12-18 01:00:31 -0500

    Add netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
       },
     },
     "gatsby-plugin-react-helmet",
+    "gatsby-plugin-netlify",
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
