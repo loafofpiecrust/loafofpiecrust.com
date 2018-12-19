408c7d94586904572a3aa852548adaf5586bd966
Taylor Snead <taylorsnead@gmail.com>
2018-12-19 13:45:04 -0500

    :alembic: Try using frontmatter for story titles

--- src/components/story.tsx
+++ src/components/story.tsx
@@ -5,11 +5,18 @@ import Layout from "./layout/layout"
 import { Flex } from "styles/system/flex";
 
 export default ({ pageContext, data: { mdx }, ...props }) => {
-  const firstHeading = mdx.headings && mdx.headings.length ? mdx.headings[0].value : null
-  const title = mdx.frontmatter.title || firstHeading
+  const firstHeading = mdx.headings && mdx.headings.length ? mdx.headings[0] : null
+  const title = mdx.frontmatter.title || firstHeading.value
 
   return <Layout title={title}>
-    <article><MDXRenderer {...props}>{mdx.code.body}</MDXRenderer></article>
+    <article>
+      {!firstHeading || firstHeading.depth > 1 ?
+        <h1>{title}</h1>
+      : null}
+      
+      <MDXRenderer {...props}>{mdx.code.body}</MDXRenderer>
+    </article>
+
     <Flex as="nav" justifyBetween>
       <a href={pageContext.previous}>Prev</a>
       <a href={pageContext.next}>Next</a>
@@ -18,13 +25,13 @@ export default ({ pageContext, data: { mdx }, ...props }) => {
 }
 
 export const pageQuery = graphql`
-query($id: String!) {
-  mdx(id: { eq: $id }) {
-    frontmatter {
-      title
+  query($id: String!) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+      }
+      headings { value }
+      code { body }
     }
-    headings { value }
-    code { body }
   }
-}
 `
